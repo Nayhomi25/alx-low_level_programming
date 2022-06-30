7dbf62818e887d563b119a67ee2fab2c66021b00
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-01 00:32:18 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -14,12 +14,11 @@ int main(void)
 	long long alonglong;
 	float afloat;
 
-	/* sizeof() is used to evaluate the size of the various variables*/
-	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
 }
