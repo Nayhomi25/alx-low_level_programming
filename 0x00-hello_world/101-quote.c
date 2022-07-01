a8a49f1ad7539d9c6dbed5707ec68941137cbbc9
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-01 01:19:33 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/** main - Entry point
+/**
+ * main - Entry point
  *
- * Description: 'print to output without using printf or puts'
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return: Always return 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
