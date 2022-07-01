5843ceaa4a18334d69ba0c949ffea4e665380a18
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-06-30 17:10:16 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /** main - Entry point
+ *
+ * Description: print to output without using printf or puts
  *
  * Return: Always return 0 (success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-       return (1);	
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+       return (1);
 }
