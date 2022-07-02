b73d990094a288035d6f1954978f78250505f8fa
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-02 15:17:23 -0700

    fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,12 @@ int main(void)
 
 	for (t = 0; t <= 9; t++)
 	{
-		for (o = 0; o <= 9; o++)
+		for (o = t + 1; o <= 9; o++)
 		{
 			putchar(t + '0');
 			putchar(o + '0');
 
-			if (!(t == 9 && o == 9))
+			if (!( t == 9 && o ==9))
 			{
 				putchar(',');
 				putchar(' ');
