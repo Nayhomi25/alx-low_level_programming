d8765fdf9db2e6b2401d088367715e66cb4aae0a
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-02 14:58:15 -0700

    combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+	int t;
+	int o;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (o = 0; o <= 9; o++)
+		{
+			putchar(t + '0');
+			putchar(o + '0');
+
+			if (t < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
+}
