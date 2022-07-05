ee6be95a7da20cab98dd7412e46a42a6b207b3f3
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 14:31:22 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 12);
+
+	return (1);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02-FUNCTIONS_NESTED_LOOPS
+
+# A Diredctory containing C files for Alx functions and nested loops project
