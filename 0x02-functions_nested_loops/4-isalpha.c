e69a60f1ced7658ac6494311545b539ae1f21196
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 18:32:16 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for an alphabetic character
+ *
+ * @c: character to check for
+ *
+ * Return: return 1 if successful, return 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n');
+}
