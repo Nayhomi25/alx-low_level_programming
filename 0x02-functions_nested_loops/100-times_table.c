6612b11e717d53d1f2631d8db7bb206b5aa0124c
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-06 22:43:51 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,43 +7,40 @@
  *
  */
 
-void print_times_table(int n);
+void print_times_table(int n)
 {
 	int a, b, d;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+	for (a = 0; a <= n; a++)
+	{
+	_putchar(0);
+	for (b = 1; b <= n; b++)
+	{
+		d = a * b;
+		_putchar(',');
+		_putchar(' ');
+		if (d <= 9)
 		{
-			_putchar(0);
-
-			for (b = 1; b <= n; b++)
-			{
-				d = a * b;
-
-				_putchar(',');
-				_putchar(' ');
-
-				if (d <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(d + '0');
-				}
-				else if (d <= 99)
-				{
-					_putchar(' ');
-					_putchar((d / 10) + '0');
-					_putchar((d % 10) + '0');
-				}
-				else
-				{
-					_putchar(((d / 100) % 10) + '0');
-					_putchar(((d / 10) % 10) + '0');
-					_putchar((d % 10) + '0');
-				}
-			}
-			_putchar('\n');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(d + '0');
 		}
+		else if (d <= 99)
+		{
+		_putchar(' ');
+		_putchar((d / 10) + '0');
+		_putchar((d % 10) + '0');
+		}
+		else
+		{
+		_putchar(((d / 100) % 10) + '0');
+		_putchar(((d / 10) % 10) + '0');
+		_putchar((d % 10) + '0');
+		}
+	}
+	_putchar('\n');
+	}
 	}
 }
