1ee0b8b54dffac0461623fcc966c18e2f6fb8e91
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 19:23:31 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ *
+ * @n: the number inputed
+ *
+ * Return: last digit
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10
+
+		if (ld < 0)
+		{
+			_putchar(-ld + '0');
+			return (-l);
+		}
+		else
+		{
+			_putchar(l + '0');
+			return (l);
+		}
+}
