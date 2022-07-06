113c1e596fe7b8fac10caa4afab11cbe1761aaaa
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 20:23:04 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98 followed ny a newline
+ *
+ * @n: input number to begin count
+ */
+
+void print_to_98(int n);
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+
+		}
+	}
+}
