a1dc46c0c733e4e327d96e68d1181c1354977502
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 18:34:46 -0700

    fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,7 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
@@ -19,5 +19,5 @@ int _isalpha(int c)
 		return (0);
 	}
 
-	_putchar('\n');
+		_putchar('\n');
 }
