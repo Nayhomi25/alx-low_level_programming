a56484ee194f20619729f85568762c343ae64ef6
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-06 16:39:32 -0700

    50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numers beginning
+ * with 1 and 2.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int fib;
+	long unsigned int n1 = 0, n2 = 1, n3;
+
+	for (fib = 0; fib < 50; fib++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+
+		n1 = n2;
+		n2 = n3;
+
+		if (fib == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+
+	}
+	return (0);
+}
