bcaa76ff9c811203b646850b76000c52888b0bfb
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-06 04:24:11 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,21 @@
  * @n: input number to begin count
  */
 
-void print_to_98(int n);
+void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n > 98)
 	{
-		while (n > 98)
+	for (; n > 98; n--)
 		{
-
+		printf("%d, ", n);
 		}
 	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+		printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
