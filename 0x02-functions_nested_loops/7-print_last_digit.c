8eb04d6fb5beb78517555227b8139a168bdaf427
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-05 19:25:41 -0700

    incorrect declarations

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,14 +13,14 @@ int print_last_digit(int n)
 {
 	int ld = n % 10
 
-		if (ld < 0)
+		if (ld < 0);
 		{
 			_putchar(-ld + '0');
-			return (-l);
+			return (-ld);
 		}
 		else
 		{
-			_putchar(l + '0');
-			return (l);
+			_putchar(ld + '0');
+			return (ld);
 		}
 }
