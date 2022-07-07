f3976b457045ed237ed03e47cf59e6a8349444ef
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-07 21:46:42 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,18 +11,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a >= b && b >= c)
-	{
+	int largest;
+
+	if (a >= b && a >= c)
+		{
 		largest = a;
-	}
-	else if (b >= a && a >= c)
-	{
+		}
+	else if (b >= a && b >= c)
+		{
 		largest = b;
-	}
+		}
 	else
-	{
+		{
 		largest = c;
-	}
+		}
 
 	return (largest);
 }
