ec22a33e125f26f427d3ed79a42d5517e5e653c7
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-08 16:18:33 -0700

    print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,8 +12,7 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
-	}
+
 
 		for (h = 0; h <= size; h++)
 		{
@@ -27,5 +26,6 @@ void print_square(int size)
 				_putchar('\n');
 			}
 		}
+	}
 	_putchar('\n');
 }
