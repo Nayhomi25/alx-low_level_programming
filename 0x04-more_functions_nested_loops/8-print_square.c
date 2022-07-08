10e5fb207486b59d05e721c12d4fd10f5fb60497
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-09 00:20:55 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,22 +10,18 @@ void print_square(int size)
 {
 	int h, b;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-
-
-		for (h = 0; h <= size; h++)
+		for (h = 0; h < size; h++)
 		{
-			for (b = 0; b <= size; b++)
-			{
+			for (b = 0; b < size; b++)
 				_putchar('#');
-			}
 
 			if (h == size - 1)
-			{
-				_putchar('\n');
-			}
+				continue;
+			_putchar('\n');
 		}
 	}
+
 	_putchar('\n');
 }
