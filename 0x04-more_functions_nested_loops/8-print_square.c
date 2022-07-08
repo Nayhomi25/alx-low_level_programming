e2f94b2bd569cf1086e1e727d089a1bb4e735a61
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-08 16:09:44 -0700

    print a square using the # sign

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # sign
+ * @size: the size of the square to be printed
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int h, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+		for (h = 0; h <= size; h++)
+		{
+			for (b = 0; b <= size; b++)
+			{
+				_putchar('#');
+			}
+
+			if (h == size - 1)
+			{
+				_putchar('\n');
+			}
+		}
+	_putchar('\n');
+}
