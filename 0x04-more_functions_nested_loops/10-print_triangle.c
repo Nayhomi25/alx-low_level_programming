092a8ae00be99fe61af7cc3dfcc916dcdfb6cc6c
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-08 18:57:07 -0700

    print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle out of the # sign
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			if (i == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
