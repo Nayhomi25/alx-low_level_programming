a1b5d4b45a0173289507ae6956ad303c4b36ba63
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-13 08:55:33 -0700

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,15 @@ void print_rev(char *s)
 {
 	int lent = 0, index;
 
-	while (s[index])
+	while (s[index++])
 	{
 		lent++;
 	}
 
-	for (index = lent - 1; inde >= 0; index--)
+	for (lent = index - 1; index >= 0; index--)
 	{
 		_putchar(s[index]);
 	}
-
 	_putchar('\n');
 
 }
