35f85f413f0d4d61a6b7efb5ccfc7b156513626d
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-20 14:31:02 -0700

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string to be reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i, l;
+	char temp;
+
+	if (i < l / 2)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tempo;
+		i++;
+	}
+	_putchar(*s);
+}
