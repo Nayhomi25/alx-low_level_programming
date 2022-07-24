86e67a340fc752435c76c08b9358345efb83b60d
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-07-24 15:25:13 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc - argument count
+ * @argv - pointer to argument strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
