37ce36f943a2e7dc980c7215fa622b056ad7a44c
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-08-02 11:25:28 -0700

    A struct named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new struct named dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
