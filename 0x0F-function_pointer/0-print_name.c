e5290d11db1cdd30fe32f3d81b35da2a2e998b84
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-08-03 14:57:20 -0700

    pointers to functions

---
+++ 0x0F-function_pointer/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != '\0' && name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
