d7ea9a5b4929d018e64e14b21b4ba17e7c3b61f7
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-08-06 00:05:19 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,17 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct typ - Struct operators
+ *
+ * @c: Char
+ * @tp: The function associated
+ */
+typedef struct typ
+{
+	char *c;
+	void (*tp)(va_list var);
+} typ_t;
 #endif
