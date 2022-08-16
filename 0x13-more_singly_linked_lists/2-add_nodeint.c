8800f8e3f54eecdff84143f668cae06a02530440
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-08-16 01:02:28 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
  * Return: the address of the new element or null if it failed
  */
 
-size_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
